98e458cb84aaf26e4cba90e7005a5f7a5a3e994d
anwesh-sarkar <anwesh.sarkar@gmail.com>
2021-02-06 17:18:05 -0500

    Removed describe

--- step-definitions/sample.test.js
+++ step-definitions/sample.test.js
@@ -14,12 +14,3 @@ defineFeature(feature, (test) => {
     });
   });
 });
-
-// describe("Google", () => {
-//   beforeAll(async () => {
-//     await page.goto("https://google.com");
-//   });
-//   it("should be titled Google", async () => {
-//     await expect(page.title()).resolves.toMatch("Google");
-//   });
-// });
